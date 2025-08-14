a704e974d2550fab8f00dd651bbb64b7ec2ac296
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2025-08-14 18:12:07 +0800

    [Create] UI uitls

---
+++ src/utils/ui.ts
@@ -0,0 +1,6 @@
+export const pixelatedBorder = (size: number, color: string) => {
+    return `-${size}px 0 0 0 ${color}, 
+            -${size}px 0 0 0 ${color}, 
+            0 -${size}px 0 0 ${color}, 
+            0 -${size}px 0 0 ${color}`;
+}
