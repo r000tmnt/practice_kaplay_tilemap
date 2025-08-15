ff630e61f96aee6bdd6254f2ce503cc4645fa608
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2025-08-15 16:23:24 +0800

    [Update] Get custom properties from map data. Append the properties into Gameobj for "exit". Store the direction with order on collide.

--- src/scene/game.ts
+++ src/scene/game.ts
@@ -11,11 +11,13 @@ const {
     pos, 
     sprite,
     scale,    
+    rotate,
     anchor,
     area,
     scene, 
     loadSprite, 
     loadSpriteAtlas,
+    loadShaderURL,
     body,
     opacity,
     go, 
@@ -27,6 +29,7 @@ const {
     getCamPos,
     setCamScale,
     getCamScale,
+    getAsset,
     onUpdate,
     onClick,
     isKeyDown,
@@ -39,6 +42,18 @@ const {
 
 let player : GameObj = {} as GameObj
 let mapArrows : TweenController[] = []
+/**
+ * If player leaves the map and direction. Mark with num pad notation.
+ * @value 8 - top
+ * @value 6 - right
+ * @value 2 - down
+ * @value 4 - left
+ * @value 0 - null
+ * @PS Floating number means the direction with multiple exits 
+ */
+let exitTouched: number = 0
+
+let ready = false
 
 // region Init Game
 export default function initGame(){
@@ -55,94 +70,110 @@ export default function initGame(){
         // Reference: https://jslegenddev.substack.com/p/how-to-use-tiled-with-kaboomjs
         loadSpriteAtlas('character/swordsman_spritesheet.png', 'character/swordsman_spritesheet.json')
 
-        loadSprite('map', 'bg/test_map.png')
-
         loadSprite('arrow', 'ui/arrow.png')
 
         loadFont('bebasNeue_regular', 'font/BebasNeue-Regular.ttf', { outline: 4 })
 
-        const mapData = await (await fetch('bg/test_map.json')).json()
-        const map = add([pos(0, 0)])
+        loadShaderURL("fadeTransition", null, 'shaders/fade_transition.frag')
 
-        const { width, height, tilewidth } = mapData
-
-        setCamPos(map.pos.x + ((tilewidth * width) / 2), map.pos.y + ((tilewidth * height) / 2))
-        setCamScale(5)
-
-        map.add([sprite("map"), pos(0, 0), layer('bg')])
-
-        for(const layer of mapData.layers){
-            if (layer.type === "tilelayer") continue;
-
-            if (layer.name === "colliders") {
-                for (const object of layer.objects) {
-                    map.add([
-                        area({ shape: new k.Rect(k.vec2(0), object.width, object.height)   }),
-                        body({ isStatic: true }),
-                        pos(object.x, object.y),
-                        // tags
-                        "pit"
-                    ]);
-                }
-                continue;
-            }    
-
-            if (layer.name === 'exits'){
-                let index = -1
-                for (const object of layer.objects) {
-                    index += 1
-                    map.add([
-                        area({ shape: new k.Rect(k.vec2(0), object.width, object.height)   }),
-                        body(),
-                        pos(object.x, object.y),
-                        // tags
-                        "exit"
-                    ]);
-
-                    const arrow = map.add([
-                        sprite('arrow'),
-                        opacity(1),
-                        anchor('center'),
-                        pos(object.x + (tilewidth /2) - 1, object.y - 10),
-                        // tags
-                        "arrow"
-                    ])
-
-                    setMapArrow(arrow, true, index)
-                }
-                continue;
-            }
-            
-            if (layer.name === "positions") {
-                for (const object of layer.objects) {
-                    if (object.name === "player") {
-                        player = map.add([
-                            sprite("player", { frame: 2 }), // idle frame of the player sprite
-                            area(),
-                            body(),
-                            pos(object.x, object.y),
-                            {
-                                speed: 75,
-                                step: 0
-                            },
-                            // tags
-                            "player"
-                        ]);
-                        console.log('player', player)
-                        setControl(width * tilewidth, height * tilewidth)
-                        continue;
-                    }
-                }
-            }            
-        }
-
-        console.log(map)
+        setMap('test_map')
     })
     
     go('game')
 }
 // endregion
 
+const setMap = async(name: string) => {
+    if(!getAsset(name)){
+        loadSprite(name, `bg/${name}.png`)
+    }
+
+    const mapData = await (await fetch(`bg/${name}.json`)).json()
+    const map = add([pos(0, 0)])
+
+    const { width, height, tilewidth } = mapData
+
+    setCamPos(map.pos.x + ((tilewidth * width) / 2), map.pos.y + ((tilewidth * height) / 2))
+    setCamScale(5)
+
+    map.add([sprite(name), pos(0, 0), layer('bg')])
+
+    for(const layer of mapData.layers){
+        if (layer.type === "tilelayer") continue;
+
+        if (layer.name === "colliders") {
+            for (const object of layer.objects) {
+                map.add([
+                    area({ shape: new k.Rect(k.vec2(0), object.width, object.height)   }),
+                    body({ isStatic: true }),
+                    pos(object.x, object.y),
+                    // tags
+                    "obstacle"
+                ]);
+            }
+            continue;
+        }    
+
+        if (layer.name === 'exits'){
+            let index = -1
+            for (const object of layer.objects) {
+                index += 1
+                map.add([
+                    area({ shape: new k.Rect(k.vec2(0), object.width, object.height)   }),
+                    body(),
+                    pos(object.x, object.y),
+                    // tags
+                    "exit",
+                    // Get direction from name
+                    {
+                        direction: Number(object.name),
+                        linked: object.properties.find(param => param.name === 'linked').value,
+                        map: object.properties.find(param => param.name === 'map').value
+                    }
+                ]);
+
+                // TODO - Rotate the sprite if needed
+                const arrow = map.add([
+                    sprite('arrow'),
+                    opacity(1),
+                    anchor('center'),
+                    pos(object.x + (tilewidth /2) - 1, object.y - 10),
+                    // tags
+                    "arrow"
+                ])
+
+                setMapArrow(arrow, true, index)
+            }
+            continue;
+        }
+        
+        if (layer.name === "positions") {
+            for (const object of layer.objects) {
+                if (object.name === "player") {
+                    player = map.add([
+                        sprite("player", { frame: 2 }), // idle frame of the player sprite
+                        area(),
+                        body(),
+                        pos(object.x, object.y),
+                        {
+                            speed: 75,
+                            step: 0
+                        },
+                        // tags
+                        "player"
+                    ]);
+                    console.log('player', player)
+                    setControl(width * tilewidth, height * tilewidth)
+                    continue;
+                }
+            }
+        }            
+    }
+
+    console.log(map)    
+}
+
+// #region Player control
 const setControl = (mapWidth: number, mapHeight: number) => {
     player.onUpdate(() => {
         const menuOpen = store.getState().game.menuOpen
@@ -190,11 +221,6 @@ const setControl = (mapWidth: number, mapHeight: number) => {
         }
     })
 
-    player.onCollide("exit", (any) => {
-        console.log('onCollide', any)
-        console.log('player leaveing the map')
-    })
-
     // onClick('player', (sprite) => {
     //     console.log('player', sprite)
     // })
@@ -221,7 +247,40 @@ const setControl = (mapWidth: number, mapHeight: number) => {
             })
         }
     })
+
+    // #region Player exit
+    player.onCollide("exit", (exit: GameObj) => {
+        console.log('onCollide', exit)
+        console.log('player leaveing the map')
+
+        // Go to the pointed position
+        if(exit.linked > 0){
+            exitTouched = exit.linked 
+        }else{
+            // Go to the relative position
+            switch(true){
+                case exit.direction >= 8: // top
+                    exitTouched = exit.direction - 6
+                break;
+                case exit.direction >= 6: // right
+                    exitTouched = exit.direction - 2
+                break;
+                case exit.direction >= 2: // down
+                    exitTouched = exit.direction + 6
+                break;
+                case exit.direction >= 4: // left
+                    exitTouched = exit.direction + 2
+                break;                                        
+            }               
+        }
+
+        // TODO - Map transition
+        // TODO - Destroy game objects when the screen black out
+        // TODO - Reveal the screen when everything is ready
+    })
+    // #endregion    
 }
+// #endregion
 
 const checkPosition = () => {
     const worldPos = player.worldPos()
@@ -260,3 +319,4 @@ const checkStep = () => {
         player.step = 0
     }
 }
+
