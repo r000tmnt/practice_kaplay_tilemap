363939a9f700bf7cb452238b84c48d664ed4a3a6
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2025-08-21 17:40:41 +0800

    [Update] New state textLabel to display item in dialogue component

--- src/store/game.ts
+++ src/store/game.ts
@@ -3,16 +3,20 @@ import { createSlice } from "@reduxjs/toolkit";
 const gameSlice = createSlice({
     name: 'game',
     initialState: {
-        menuOpen: 0
+        menuOpen: 0,
+        textLabel: [],
     },
     reducers : {
         setMenu: (state, action) => {
             state.menuOpen = action.payload
+        },
+        setTextLabel: (state, action) => {
+            state.textLabel = action.payload
         }
     }
 })
 
 export const { 
-    setMenu
+    setMenu, setTextLabel
 } = gameSlice.actions
 export default gameSlice.reducer
