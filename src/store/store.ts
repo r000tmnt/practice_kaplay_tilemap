47cd583686d8eef923635d6e44f0071cbd57f68c
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2025-08-21 17:41:07 +0800

    [Update] Import dialogue reducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import gameReducer from './game';
 import settingReducer from './setting';
+import dialogueReducer from './dialogue';
 
 const store = configureStore({
     reducer: {
         game: gameReducer,
-        setting: settingReducer
+        setting: settingReducer,
+        dialogue: dialogueReducer
     }
 })
 
