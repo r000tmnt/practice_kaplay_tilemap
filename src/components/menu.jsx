2bed4ab848b844ca8bdc460259ef21d372e15148
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2025-08-26 15:07:36 +0800

    [Update] Import item menu component

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -3,27 +3,23 @@ import { useDispatch, useSelector } from "react-redux";
 import { pixelatedBorder } from '../utils/ui'
 import { setMenu, setList } from "../store/game"
 import store from "../store/store";
+import { ITEMFILTER } from "../utils/ui"
 
 import MenuArrow from './menuArrow'
+import ItemMenu from "./itemMenu";
 
 const MEMUITEM = [
     'ITEM', 'SKILL', 'TEAM', 'STATUS', 'SAVE', 'LOAD', 'OPTION'
 ]
 
-const ITEMFILTER = [
-    'ALL', 'EQUIP', 'CONSUME', 'MATERIAL', 'STORY'
-]
-
 export default function Menu() {
     const menuOpen = useSelector(state => state.game.menuOpen)
-    const innerMenuOpen = useSelector(state => state.game.innerMenuOpen)
     const gameWidth = useSelector(state => state.setting.width)
     const gameHeight = useSelector(state => state.setting.height)
     const scale = useSelector(state => state.setting.scale)
     const uiOffsetV = useSelector(state => state.setting.uiOffsetV)
     // const uiOffsetH = useSelector(state => state.setting.uiOffsetH) 
     const units = useSelector(state => state.game.units)  
-    const itemList = useSelector(state => state.game.items)
     const inventory = useSelector(state => state.game.inventory)
     const [menuIndex, setMenuIndex] = useState(0)
     const [innerMenuIndex, setInnerMenuIndex] = useState(0)
@@ -59,14 +55,30 @@ export default function Menu() {
                         setInnerMenuIndex(preState => (preState === 0)? 0 : preState - 1)
                         return
                     }
-                    setMenuIndex(preState => (preState - 2) < 0? preState : preState - 2)
+                    setMenuIndex(preState => {
+                        if((preState - ITEMFILTER.length) <= 1){
+                            return 0
+                        }else{
+                            return preState - 2
+                        }
+                    })
                 }
                 if($event.key === 'ArrowDown'){
                     if(store.getState().game.innerMenuOpen > 0){
                         setInnerMenuIndex(preState => (preState === (units.length - 1))? preState : preState + 1)
                         return
                     }
-                    setMenuIndex(preState => preState + 2)
+                    const itemList = store.getState().game.items
+                    setMenuIndex(preState => {
+                        if(preState < ITEMFILTER.length){
+                            return ITEMFILTER.length
+                        }
+                        if(preState + 2 > (ITEMFILTER.length + (itemList.length - 1))){
+                            return ITEMFILTER.length + (itemList.length - 1)
+                        }else{
+                            return preState + 2
+                        }    
+                    })
                 }
                 if($event.key === 'ArrowRight'){
                     if(store.getState().game.innerMenuOpen > 0) return
@@ -83,7 +95,10 @@ export default function Menu() {
                         dispath(setMenu({ type: 2, value: 0 }))
                     }else{
                         // Close parent menu
-                        setMenuIndex(0)
+                        store.dispatch(
+                            setMenu({type: 1, value: 1})
+                        )                           
+                        setMenuIndex(menuOpen - 1)
                     }
                 }
                 if($event.key === 'Enter'){
@@ -110,7 +125,6 @@ export default function Menu() {
                 setMenuIndex(0)
             break;
             case 2:
-                setMenuIndex(ITEMFILTER.length)
                 import('../data/items.json').then(data => {
                     console.log(data)
                     const items = []
@@ -204,114 +218,14 @@ export default function Menu() {
             </ul>  
 
             {/*  ITEM  */}
-            <div 
-                className={`menu sub_menu hide ${menuOpen === 2? 'show' : ''}`} 
-                style={{ padding: `${(8 * Math.floor(scale * 10)) / 2}px`, fontSize: `${8 * (scale * 10)}px`}}>
-                <div className="flex filter">
-                    {
-                        ITEMFILTER.map((filter, index) => (
-                            <div className="item flex" key={index}>
-                                { menuOpen === 2 && menuIndex === index ? 
-                                    <MenuArrow /> : null
-                                }
-                                <span>{filter}</span>
-                            </div>
-                        ))
-                    }
-                </div>
-                <div 
-                    className="items"
-                    style={{
-                        rowGap: scale * 10 + 'px',
-                        columnGap: scale * 20 + 'px',
-                    }}>
-                    {
-                        itemList.map((item, index) => 
-                        <div 
-                            className="item flex" 
-                            key={index} 
-                            style={{ border: `${scale * 10}px solid transparent`, }}
-                            onMouseOver={() => {
-                                if(innerMenuOpen > 0) return
-                                setMenuIndex(index + ITEMFILTER.length)
-                            }}
-                            onClick={() => {
-                                if(item.type === 1) dispath(setMenu({ type: 2, value: 1 }))
-                            }}>
-                            { menuOpen === 2 && menuIndex === (index + ITEMFILTER.length)? 
-                                <MenuArrow /> : null
-                            }                        
-                            <div className="flex" style={{
-                                width: `${gameWidth * 0.3}px`,
-                                marginLeft: 'auto',
-                                justifyContent: 'space-around'
-                            }}>
-                                <span>{ item.name }</span>
-                                <span style={{ marginLeft: 'auto' }}>{ item.amount }</span>                                
-                            </div>
-                        </div>)
-                    }
-                </div>
-                <div className="bottom">
-                    <div className="desc" style={{ 
-                        width: '100%',
-                        boxShadow: pixelatedBorder(scale * 10, 'black'), 
-                        padding: `${scale * 10}px`,
-                        boxSizing: 'border-box'
-                    }}>
-                        { innerMenuOpen > 0? 
-                            <div className="innerMenu">
-                                { innerMenuOpen === 1?
-                                    <div className="flex flex-col">
-                                        { units.map((unit, index) => 
-                                            <div 
-                                                className="flex" 
-                                                key={index} 
-                                                onMouseOver={() => setInnerMenuIndex(index)}
-                                                onClick={() => { console.log('use item') }}>
-                                                { menuOpen === 2 && innerMenuIndex === index? 
-                                                    <span style={{ position: 'absolute', zIndex: 11 }}>
-                                                        <MenuArrow /> 
-                                                    </span>
-                                                    : null
-                                                }                                                
-                                                <div 
-                                                    className="flex w-full" 
-                                                    style={{
-                                                        whiteSpace: 'nowrap',
-                                                        justifyContent: 'space-around'
-                                                    }} >
-                                                    <div>{ unit.name }</div>
-                                                    <div>HP {unit.attribute.hp}/{unit.attribute.maxHp}</div>
-                                                    <div>MP {unit.attribute.mp}/{unit.attribute.maxMp}</div>
-                                                </div>                                                
-                                            </div>
-                                            ) 
-                                        }
-                                    </div> : null
-                                }
-                            </div> : itemList[menuIndex - ITEMFILTER.length]? itemList[menuIndex - ITEMFILTER.length].desc : ''
-                        }
-                    </div>
-                    <button style={{ 
-                        width: '100%', 
-                        backgroundColor: 'white', 
-                        margin: `${scale * 10}px 0`,
-                        color: 'black', 
-                        fontSize: `${8 * (scale * 10)}px` 
-                    }} onClick={() => {
-                        if(innerMenuIndex > 0){
-                            // Close inner menu
-                            dispath(setMenu({ type: 2, value: 0 }))
-                            setInnerMenuIndex(0)
-                        }else{
-                            // Close parent menu
-                            setMenuIndex(0)
-                            dispath(setList({ type: 2, data: [] }))                            
-                        }
-                    }}>BACK</button>
-                </div>
-            </div>
+            { menuOpen === 2?
+                <ItemMenu 
+                    menuIndex={menuIndex}
+                    innerMenuIndex={innerMenuIndex}
+                    setMenuIndex={setMenuIndex}
+                    setInnerMenuIndex={setInnerMenuIndex} 
+                /> : null
+            }
 
             {/*  SKILL  */}
             <div className={`menu sub_menu hide ${menuOpen === 3? 'show' : ''}`} style={{ padding: `${(8 * Math.floor(scale * 10)) / 2}px` }}>
