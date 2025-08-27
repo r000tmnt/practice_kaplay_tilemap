3d3f7a9c8969312427fba0c6139ea9fcc16a4ef6
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2025-08-27 16:44:38 +0800

    [Update] playerPositionRef

--- src/utils/ui.ts
+++ src/utils/ui.ts
@@ -8,3 +8,8 @@ export const pixelatedBorder = (size: number, color: string) => {
 export const ITEMFILTER = [
     'ALL', 'EQUIP', 'CONSUME', 'MATERIAL', 'STORY'
 ]
+
+export const playerPositionRef = [
+  // x, y in percentage
+  [0.7, 0.6], [0.7, 0.7], [0.8, 0.55], [0.8, 0.65], [0.8, 0.75]
+]
