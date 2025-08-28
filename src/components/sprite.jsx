6aa82f18f8ed54178b190d9011577bbb6f0da825
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2025-08-28 18:18:15 +0800

    [Create] Sprite component

---
+++ src/components/sprite.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react"
+
+/**
+ * Display sprite in sprite sheet
+ * @param width - The width of sprite
+ * @param height - The height of the sprite
+ * @param image - The path of the sprite sheet
+ * @param position - Css background-position to locate the sprite
+ * @param custom - Optional parammeters for customization
+ * @param custom.style - An object of css styles
+ * @param custom.className - A string of class name(s)
+ * @param custom.content - Anything to display within the component
+ * @param custom.onMouseOver - A function to trigger when cursor hover on the component
+ * @param custom.onClick - A function to trigger when cursor click on the component
+ * @returns 
+ */
+export default function Sprite({
+    width, 
+    height, 
+    image,
+    position,
+    custom={}
+}){
+    const { style, className, content, onMouseOver, onClick } = custom
+
+    const [customStyle, setCustomStyle] = useState({
+        position: 'absolute',
+        backgroundImage: `url(${image})`,
+        backgroundPosition: position,
+        width: `${width}px`,
+        height: `${height}px`,
+        backgroundSize: 'auto',        
+    })
+
+    useEffect(() => {
+        if(style){
+            setCustomStyle(preState => {
+                return {
+                    ...preState,
+                    ...style
+                }
+            })
+        }
+    }, [])
+
+    return(
+        <div 
+        className={`sprite ${className}`}
+        style={customStyle}
+        onMouseOver={() => onMouseOver?? onMouseOver()}
+        onClick={() => onClick?? onClick()}
+        >
+            { content }    
+        </div> 
+    )
+}
