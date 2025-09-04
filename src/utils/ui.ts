37c4cf4072943dc1ff712a72704b65863b973735
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2025-09-04 10:41:55 +0800

    [Update] Language list

--- src/utils/ui.ts
+++ src/utils/ui.ts
@@ -9,6 +9,10 @@ export const ITEMFILTER = [
     'ALL', 'EQUIP', 'CONSUME', 'MATERIAL', 'STORY'
 ]
 
+export const LANGUAGELIST = [
+    'ENG', 'ZH-TW'
+]
+
 export const playerPositionRef = [
   // x, y in percentage
   [0.7, 0.6], [0.7, 0.7], [0.8, 0.55], [0.8, 0.65], [0.8, 0.75]
