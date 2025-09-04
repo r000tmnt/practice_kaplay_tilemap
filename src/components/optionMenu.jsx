d5c3852b9e53e05296583e09d30ee2d154b3c08a
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2025-09-04 10:40:35 +0800

    [Update] Set up mouse control for option menu

--- src/components/optionMenu.jsx
+++ src/components/optionMenu.jsx
@@ -1,24 +1,50 @@
 import { useSelector, useDispatch } from "react-redux"
 import { pixelatedBorder } from "../utils/ui"
-import { useEffect } from "react"
+import { useEffect, useState, useImperativeHandle, forwardRef  } from "react"
 
-const LANGUAGELIST = [
-    'ENG', 'ZH-TW'
-]
+import { LANGUAGELIST } from '../utils/ui'
 
-export default function OptionMenu({
+const OptionMenu = forwardRef(({
     menuIndex,
     setMenuIndex,
     enterPressed,
     setEnterPressed
-}){
+}, ref) => {
     const scale = useSelector(state => state.setting.scale)
+    const [bgmVolumn, setBgmVolumn] = useState(100)
+    const [seVolumn, setSEVolumn] = useState(100)
+    const [currentLang, setCurrentLang] = useState(LANGUAGELIST[0])
+
+    useImperativeHandle(ref, () => ({
+        options: [
+            {
+                name: 'BGM',
+                value: bgmVolumn,
+                set: (v) => setBgmVolumn(v)
+            },
+            {
+                name: 'SE',
+                value: seVolumn,
+                set: (v) => setSEVolumn(v)
+            },
+            {
+                name: 'lang',
+                value: currentLang,
+                set: (v) => setCurrentLang(v)
+            }
+        ]
+    }))
 
     const setMenuPosition = ($el) => {
         if($el) $el.classList.add('show')
     }    
 
-    useEffect(() => {}, [])
+    useEffect(() => {
+        if(enterPressed){
+            // ...
+            setEnterPressed(false)
+        }
+    }, [enterPressed])
 
     return(
         <div 
@@ -29,17 +55,52 @@ export default function OptionMenu({
             <ul>
                 <li>
                     <div className="sub_title">BGM</div>
-                    <input className="w-full" type="range" />
+                    <input 
+                        className="w-full" 
+                        type="range" 
+                        value={bgmVolumn} 
+                        onMouseOver={() => setMenuIndex(0)}
+                        onChange={(v) => setBgmVolumn(v)} 
+                        style={{ 
+                            boxShadow: (menuIndex === 0)? pixelatedBorder(scale * 5, 'grey') : 'unset',
+                        }}
+                    />
                 </li>
                 <li>
                     <div className="sub_title">SE</div>
-                    <input className="w-full" type="range" />
+                    <input 
+                        className="w-full" 
+                        type="range" 
+                        value={seVolumn} 
+                        onMouseOver={() => setMenuIndex(1)}
+                        onChange={(v) => setSEVolumn(v)} 
+                        style={{ 
+                            boxShadow: (menuIndex === 1)? pixelatedBorder(scale * 5, 'grey') : 'unset',
+                        }}
+                    />
                 </li>
                 <li>
                     <div className="sub_title">LANGUAGE</div>
-                    <button style={{ background: 'lightgrey', fontSize: `${6 * (scale * 10)}px` }}>ENG</button>
+                    <div className="flex">
+                        {
+                            LANGUAGELIST.map((lang, index) => 
+                                <button 
+                                    style={{ 
+                                        background: (menuIndex === index + 2)? 'white' : 'lightgrey', 
+                                        fontSize: `${6 * (scale * 10)}px`,
+                                        marginRight: `${scale * 10}px`,
+                                    }}
+                                    onMouseOver={() => setMenuIndex(index + 2)}
+                                    onClick={() => setCurrentLang(lang)}
+                                    disabled={currentLang === lang}    
+                                >{lang}</button>
+                            )
+                        }
+                    </div>
                 </li>                
             </ul>
         </div>
     )
-}
+});
+
+export default OptionMenu
